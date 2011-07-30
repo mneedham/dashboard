8de7ee5c3082f6846fa06be4c224a9b4c516e41a
Mark Needham <m.h.needham@gmail.com>
2011-07-30 11:43:24 +0100

    mark: getting jade up and running

--- dashboard.js
+++ dashboard.js
@@ -1,35 +1,49 @@
+
 var http = require('http');
 var fs = require('fs');
-http.createServer(function (req, res) {
-//  res.writeHead(200, {'Content-Type': 'text/plain'});
-//  parseCsvFile('/private/tmp/foo.txt', function(rec) {
- //   res.write(rec.hash + " src/main:" + rec.main + " test/unit:" + rec.unit + " test/integration:" + rec.integration + " test/functional:" + rec.functional)
- //   res.write("\n")
- // })
-fs.readFile('RGraph/examples/node-line.html', 'utf-8', function (err, data) {
-   if (err) throw err;
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(data);
-    });  
-}).listen(1337, "127.0.0.1");
+var express = require('express')
 
-function parseCsvFile(fileName, callback){
-	var stream = fs.createReadStream(fileName, { encoding : 'ascii'})
-	stream.addListener('data', function(data){
-	  var parts = data.split('\n')
-	  parts.forEach(function(d, i){
-	    var line = d.split(":")
-	    if(line.length == 5)
-	    	callback({ hash: line[0], main: line[1], unit: line[2], integration: line[3], functional: line[4]}) 
-	  })
-	})
+var app = express.createServer();
+app.register('.html', require('jade'));
+app.use(express.static(__dirname + '/static'));
 
-	function buildRecord(str){
-	  var record = {}
-	  str.split(pattern).forEach(function(value, index){
-	    if(header[index] != '')
-	      record[header[index].toLowerCase()] = value.replace(/"/g, '')
-	  })
-	  return record
-	}
-}
+app.get('/', function(req, res){
+     res.render('index.jade', { title: 'My Site' });
+});
+
+app.listen(3000);
+
+
+// http.createServer(function (req, res) {
+// //  res.writeHead(200, {'Content-Type': 'text/plain'});
+// //  parseCsvFile('/private/tmp/foo.txt', function(rec) {
+//  //   res.write(rec.hash + " src/main:" + rec.main + " test/unit:" + rec.unit + " test/integration:" + rec.integration + " test/functional:" + rec.functional)
+//  //   res.write("\n")
+//  // })
+// fs.readFile('RGraph/examples/node-line.html', 'utf-8', function (err, data) {
+//    if (err) throw err;
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   res.end(data);
+//     });  
+// }).listen(1337, "127.0.0.1");
+// 
+// function parseCsvFile(fileName, callback){
+// 	var stream = fs.createReadStream(fileName, { encoding : 'ascii'})
+// 	stream.addListener('data', function(data){
+// 	  var parts = data.split('\n')
+// 	  parts.forEach(function(d, i){
+// 	    var line = d.split(":")
+// 	    if(line.length == 5)
+// 	    	callback({ hash: line[0], main: line[1], unit: line[2], integration: line[3], functional: line[4]}) 
+// 	  })
+// 	})
+// 
+// 	function buildRecord(str){
+// 	  var record = {}
+// 	  str.split(pattern).forEach(function(value, index){
+// 	    if(header[index] != '')
+// 	      record[header[index].toLowerCase()] = value.replace(/"/g, '')
+// 	  })
+// 	  return record
+// 	}
+// }
