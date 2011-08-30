cf5cf463d3edb6024b6dc597cc090f7f880636d4
Mark Needham <m.h.needham@gmail.com>
2011-08-30 08:55:58 +0100

    mark: adjusting tests so they use my equals method

--- test/commit_message_parser.test.js
+++ test/commit_message_parser.test.js
@@ -1,16 +1,18 @@
 var assert = require('assert'), commit_message_parser = require('commit_message_parser');
 
+Pair = commit_message_parser.Pair;
+
 module.exports = {
     'parse()': function(){
-        assert.eql(["Ken", "Alex"], commit_message_parser.parse("Ken/Alex #266 added empty authors column so that subjects appear in 3rd column"));
-        assert.eql(["Alinoor", "Mark"], commit_message_parser.parse("alinoor/mark #380 more stuff this time for chapter title"));
-        assert.eql(["Mark", "Suzuki"], commit_message_parser.parse("mark,suzuki #232 fix merge"));
-	    assert.eql(["Pat", "Sebastian"], commit_message_parser.parse("pat, sebastian: removing unnecessary line in dev properties."));
-	    assert.eql(["Charles", "Mark"], commit_message_parser.parse("charles/mark: adios to play, hello scalatra"));
+        assert.ok(Pair({ person1: "Ken", person2 : "Alex"}).equals(commit_message_parser.parse("Ken/Alex #266 added empty authors column so that subjects appear in 3rd column")));
+        assert.ok(Pair({ person1: "Alinoor", person2 : "Mark"}).equals(commit_message_parser.parse("alinoor/mark #380 more stuff this time for chapter title")));
+        assert.ok(Pair({ person1: "Mark", person2 :  "Suzuki"}).equals(commit_message_parser.parse("mark,suzuki #232 fix merge")));
+	    assert.ok(Pair({ person1: "Pat", person2 : "Sebastian"}).equals(commit_message_parser.parse("pat, sebastian: removing unnecessary line in dev properties.")));
+	    assert.ok(Pair({ person1: "Charles", person2 :  "Mark"}).equals(commit_message_parser.parse("charles/mark: adios to play, hello scalatra")));
     },
     'pairs()' : function() {
-	   assert.eql({"Fri Jun 03 2011" : [["Alex", "Charles"]]}, commit_message_parser.pairs([{message:"Alex/Charles #0 Added functional ML test", date:"Fri Jun 03 2011"}]));  
-	   assert.eql({"Fri Jun 03 2011" : [["Alex", "Charles"], ["Alinoor", "Mark"]]}, 
+	   assert.eql({"Fri Jun 03 2011" : [Pair({person1 : "Alex", person2 : "Charles"})]}, commit_message_parser.pairs([{message:"Alex/Charles #0 Added functional ML test", date:"Fri Jun 03 2011"}]));  
+	   assert.eql({"Fri Jun 03 2011" : [Pair({person1 : "Alex", person2 :  "Charles"}), Pair({person1:"Alinoor", person2: "Mark"})]}, 
 				  commit_message_parser.pairs([{message:"Alex/Charles #0 Added", date:"Fri Jun 03 2011"}, {message:"alinoor/mark #380 more stuff",date:"Fri Jun 03 2011"}]));  
 	   assert.eql({"Fri Jun 03 2011" : [["Alex", "Charles"]], "Mon Jun 06 2011" : [["Pat", "Liz"]]}, 
 				  commit_message_parser.pairs([{message:"Alex/Charles #0 Added", date:"Fri Jun 03 2011"}, {message:"pat, liz: removing stuff",date:"Mon Jun 06 2011"}]));
@@ -27,14 +29,13 @@ module.exports = {
 		assert.eql(false, commit_message_parser.hasPair("alinoor/mushtaq/mark: fix chef script for scala"))
 	},
 	'Pair()' : function() {
-		assert.ok(commit_message_parser.Pair({person1 : "Mark", person2 : "Pat"}).equals({person1 : "Mark", person2 : "Pat"}));
-		assert.ok(commit_message_parser.Pair({person1 : "Mark", person2 : "Pat"}).equals({person1 : "Pat", person2 : "Mark"}));
-		assert.ok(!commit_message_parser.Pair({person1 : "Mark", person2 : "Pat"}).equals({person1 : "Liz", person2 : "Mark"}));
+		assert.ok(Pair({person1 : "Mark", person2 : "Pat"}).equals({person1 : "Mark", person2 : "Pat"}));
+		assert.ok(Pair({person1 : "Mark", person2 : "Pat"}).equals({person1 : "Pat", person2 : "Mark"}));
+		assert.ok(!Pair({person1 : "Mark", person2 : "Pat"}).equals({person1 : "Liz", person2 : "Mark"}));
 	},
 	'unique()' : function() {
 		var markAndPat = [commit_message_parser.Pair({person1 : "Mark", person2 : "Pat"}), commit_message_parser.Pair({person1 : "Mark", person2 : "Pat"})];
 		var unique = markAndPat.unique()[0];
-		console.log(unique);
 		assert.ok(commit_message_parser.Pair({person1 : "Mark", person2 : "Pat"}).equals(unique));
 	}
 };
