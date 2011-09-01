5aec7b11665f49482592ae046720110c4af062c9
Mark Needham <m.h.needham@gmail.com>
2011-09-01 08:33:03 +0100

    mark: pulling out into its own file

---
+++ static/pairs.js
@@ -0,0 +1,35 @@
+function Pairs(data) {
+	var pairs = parsePairs(data);
+	
+    function parsePairs(data) {
+      var pairs = {};
+      $.each(data, function(key, val) {
+        $.each(val, function(person, numberOfCommits) {
+          if(!pairs[person]) {
+            pairs[person] = { timesPaired : 0, numberOfCommits : 0, lastPaired : "0" };
+          }
+
+          if(new Date(pairs[person]["lastPaired"]) < new Date(key)) {
+            pairs[person]["lastPaired"] = key;
+          }
+
+          pairs[person]["timesPaired"] += 1;
+          pairs[person]["numberOfCommits"] += numberOfCommits;
+        });              		
+      }); 
+      return pairs;
+    }
+
+    function mostPairings() {
+      var maxPairCount = 1;
+      $.each(pairs, function(person, options) {
+        if(options.timesPaired > maxPairCount) {
+           maxPairCount = options.timesPaired;
+        }
+      });
+      return maxPairCount;
+    }
+
+    return { get : pairs, mostPairings : mostPairings
+	};	
+}
