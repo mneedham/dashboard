f1560e7840c156110caf2893a392b1b4ee81a6d1
Mark Needham <m.h.needham@gmail.com>
2011-09-03 12:42:55 +0100

    mark: structuring code better

--- static/linesOfCodeGraph.js
+++ static/linesOfCodeGraph.js
@@ -1,43 +1,3 @@
-var CommitsGraphs = function() {
-	function drawGraph(container, commits, ticks) {
-	      $.jqplot(container, [commits], {
-	        seriesDefaults:{
-	          renderer:$.jqplot.BarRenderer, pointLabels:{show:true, stackedValue: true},
-	          rendererOptions: {fillToZero: true}
-	        }, 
-	        axes : {
-	          yaxis : { padMin : 0, pad: 1.1, min:0, tickOptions:{ formatString:'%.0f' } },
-	          xaxis : { padMin : 0, ticks : ticks, renderer: $.jqplot.CategoryAxisRenderer }	  
-	        }
-	      });		
-	}
-	
-	function nonZeroEntries(data) {
-		var filtered = {};	 
-	    for(var date in data) {
-	      if(data[date] > 0) filtered[date] = data[date];	  	
-	    }
-	    return filtered;		
-	}
-	
-	function init() {
-		$.getJSON('/git/commits/by-time', function(data) {
-	      var filtered = nonZeroEntries(data);
-	      var commits = _(filtered).map(function(numberOfCommits) { return numberOfCommits; });
-	      var ticks = _(filtered).map(function(_, time) { return time.toString().match(/(\d\d:00)(?=.*GMT)/g).join("-"); });
-		  drawGraph('commits-by-time', commits, ticks);	
-	    });
-
-	    $.getJSON('/git/commits/by-day', function(data) {
-	      var commits = _(data).map(function(numberOfCommits) { return numberOfCommits; });
-	      var ticks = _(data).map(function(_, day) { return day; })	
-		  drawGraph('commits-by-day', commits, ticks);
-	    });
-	}
-	
-	return { init : init };
-}
-
 var LinesOfCodeGraph = function() {
 	function drawGraph(options) {
 		var code = _(options.data).map(function(obj, idx) { return obj.main });
@@ -56,11 +16,11 @@ var LinesOfCodeGraph = function() {
 	}
 	
 	function drawRatioGraph(options) {
-		var code = _(options.data).map(function(obj, idx) { return obj.main });
-		var unit = _(options.data).map(function(obj, idx) { return obj.unit });
-		var functional = _(options.data).map(function(obj, idx) { return obj.functional });
-		var integration = _(options.data).map(function(obj, idx) { return obj.integration });
-		var system = _(options.data).map(function(obj, idx) { return obj.system });		
+		var code = _(options.data).map(function(obj) { return obj.main });
+		var unit = _(options.data).map(function(obj) { return obj.unit });
+		var functional = _(options.data).map(function(obj) { return obj.functional });
+		var integration = _(options.data).map(function(obj) { return obj.integration });
+		var system = _(options.data).map(function(obj) { return obj.system });		
 		
 		var ratio =  _(_.zip(code, unit, integration, functional, system)).map(function(values) { 
 			return (values[1].toInt() + values[2].toInt() + values[3].toInt() + values[4].toInt()) / values[0].toInt(); 
