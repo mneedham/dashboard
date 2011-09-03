7c9fd6ba2a24c1d067dd7da94af7d9e5b27e5a8e
Mark Needham <m.h.needham@gmail.com>
2011-09-03 16:07:44 +0100

    mark: got the hovering for data points working

--- static/linesOfCodeGraph.js
+++ static/linesOfCodeGraph.js
@@ -1,48 +1,25 @@
 var LinesOfCodeGraph = function() {
 	function drawGraph(options) {
-		// var code = _(options.data).map(function(obj, idx) { return obj.main });
-		// 		var unit = _(options.data).map(function(obj, idx) { return obj.unit });
+		var unit = _(options.data).map(function(obj, idx) { return [new Date(obj.time*1000), obj.unit] });
 		var functional = _(options.data).map(function(obj, idx) { return [new Date(obj.time*1000), obj.functional] });
-		// console.log(functional);
-		// functional = [["23 May 2011", 4], ["25 May 2011", 5],  ["25 May 2011", 5],  ["25 May 2011", 5]]
-		// 	// var integration = _(options.data).map(function(obj, idx) { return obj.integration });
-		// 	// var system = _(options.data).map(function(obj, idx) { return obj.system });
-		// 	
-		//  var line1=[['23-May-08', 578.55], ['20-Jun-08', 566.5], ['25-Jul-08', 480.88], ['22-Aug-08', 509.84],
-		//       ['26-Sep-08', 454.13], ['24-Oct-08', 379.75], ['21-Nov-08', 303], ['26-Dec-08', 308.56],
-		//       ['23-Jan-09', 299.14], ['20-Feb-09', 346.51], ['20-Mar-09', 325.99], ['24-Apr-09', 386.15]];
-		
-	  var plot1 = $.jqplot('git', [functional], {
-	      title:'Data Point Highlighting',
+		var code = _(options.data).map(function(obj, idx) { return [new Date(obj.time*1000), obj.main] });
+		var integration = _(options.data).map(function(obj, idx) { return [new Date(obj.time*1000), obj.integration] });		
+		var system = _(options.data).map(function(obj, idx) { return [new Date(obj.time*1000), obj.system] });		
+		var shared = _(options.data).map(function(obj, idx) { return [new Date(obj.time*1000), obj.shared] });		
+				
+				
+	  var plot1 = $.jqplot('git', [code, unit, functional, integration, system, shared ], {
+	      title:'Lines of code', series:[{label:'Code'}, {label:'Unit Tests'}, {label:'Functional Tests'}, {label:'Integration Tests'}, {label:'System Tests'}, {label:'Shared Test Code'}],
+	      legend: { show: true, location : "nw" },
 	      axes:{
-	        xaxis:{
-	          renderer:$.jqplot.DateAxisRenderer,  padMin : 0, pad:0,
-	          tickOptions:{
-	            formatString:'%b&nbsp;%#d'
-	          },  autoscale :true
-	        },
-	        yaxis:{
-	          tickOptions:{
-	            formatString:'%.0f'
-	            }, max :_(_(functional).map(function(arr) { return arr[1];})).max(), padMin : 0, pad: 1.1, min:0, 
-	        }
+	        xaxis:{ renderer:$.jqplot.DateAxisRenderer,  padMin : 0, pad:0, tickOptions:{ formatString:'%b&nbsp;%#d' },  autoscale :true },
+	        yaxis:{ tickOptions:{formatString:'%.0f'}, max :_(_(functional).map(function(arr) { return arr[1];})).max(), padMin : 0, pad: 1.1, min:0 }
 	      },
-	      highlighter: {
-	        show: true,
-	        sizeAdjust: 7.5, showMarker: false,
-	      },
-	      cursor: {
-	        show: false
-	      }
+	      highlighter: { show: true, sizeAdjust: 7.5, showMarker: false },
+	      cursor: { show: false }, 
+	      seriesDefaults: { showMarker:false, lineWidth: 1},
 	  });		
 		
-		  // 	    $.jqplot('git', [functional], {
-		  // 	      title: 'Lines of code', 
-		  // series:[{label:'Code'}, {label:'Unit Tests'}, {label:'Functional Tests'}, {label:'Integration Tests'}, {label:'System Tests'}],	
-		  // legend: { show: true, location : "nw" },
-		  // 	      seriesDefaults: { showMarker:false, lineWidth: 1},
-		  // 	      axes:{ yaxis:{padMin : 0, pad: 1.1, min:0, max : _(functional).max(), tickOptions:{ formatString:'%.0f' } }, xaxis : { renderer: $.jqplot.DateAxisRenderer,  padMin : 0, pad:0} }
-		  // 	    });			
 	}
 	
 	function drawRatioGraph(options) {
